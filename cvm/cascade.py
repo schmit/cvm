0aa60628c38646aa9199f9da40be2664f1d6977b
Sven Schmit <schmit@stanford.edu>
2015-05-20 18:20:18 -0700

    updates to cascade and svm

--- cvm/cascade.py
+++ cvm/cascade.py
@@ -10,33 +10,16 @@ from __future__ import division
 
 import numpy as np
 
+def cascade(labeledPointRDD, reducer, nmax):
+    n = labeledPointRDD.count()
+    numPartitions = int(2**(np.ceil(np.log(n / nmax)/np.log(2.0))))
+    # numLevels = int(np.round(np.log(numPartitions) / np.log(2) + 1))
+    leafsRDD = labeledPointRDD.repartition(numPartitions)
 
-class Cascade:
-    '''
-    Approximates SVM by splitting data in small chunks, running SVM locally,
-    and combining support vectors.
+    while numPartitions > 1:
+        numPartitions = int(numPartitions / 2)
+        leafsRDD = leafsRDD.mapPartitions(reducer).coalesce(numPartitions)
 
-    This could be done in parallel, though this is a serial implementation
-    '''
+    return leafsRDD.collect()
 
-    def __init__(self, model, nmax):
-        self.model = model
-        self.nmax = nmax
-
-    def train(self, labeledPointRDD):
-        n = labeledPointRDD.count()
-        numPartitions = int(2**(np.ceil(np.log(n / self.nmax)/np.log(2.0))))
-        # numLevels = int(np.round(np.log(numPartitions) / np.log(2) + 1))
-        leafsRDD = labeledPointRDD.repartition(numPartitions)
-
-        while numPartitions > 1:
-            numPartitions = int(numPartitions / 2)
-            leafsRDD = leafsRDD.mapPartitions(self.model.reduce)
-            leafsRDD = leafsRDD.coalesce(numPartitions)
-
-        self.model.train(leafsRDD.collect())
-
-
-    def predict(self, features):
-        return self.model.predict(features)
 
--- cvm/svm.py
+++ cvm/svm.py
@@ -2,16 +2,42 @@
 WIP Combines Cascade and Models
 '''
 
-from cascade import Cascade
-from models import SVMModel
+import numpy as np
+from sklearn import svm
+from pyspark.mllib.regression import LabeledPoint
+
+
+from cascade import cascade
 
 class KernelSVM:
-    def __init__(self, nmax):
-        model = SVMModel()
-        self.cascade = Cascade(model, nmax)
+    def __init__(self, kernel='rbf', C=1.0, nmax=500, gamma=1.0, degree=3):
+        self.nmax = nmax
+        self.model = svm.SVC(kernel='rbf', C=C, gamma=gamma)
 
     def train(self, labeledPoints):
-        self.cascade.train(labeledPoints)
+        labeledPoints = cascade(labeledPoints, self._reduce, self.nmax)
+        X, y = self._readiterator(labeledPoints)
+        self.model.fit(X, y)
 
     def predict(self, features):
-        return self.cascade.predict(features)
+        return self.model.predict(features)
+
+    def _reduce(self, iterator):
+        X, y = self._readiterator(iterator)
+        self.model.fit(X, y)
+        return self._returniterator(self.model.support_, X, y)
+
+    def _readiterator(self, iterator):
+        ys = []
+        xs = []
+        for elem in iterator:
+            ys.append(elem.label)
+            xs.append(elem.features)
+
+        X = np.array(xs)
+        y = np.array(ys)
+        return X, y
+
+    def _returniterator(self, indices, X, y):
+        for i in indices:
+            yield LabeledPoint(y[i], X[i])
