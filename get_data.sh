396ee8779a331b5a405b6f58d3b834e66ea58c6f
Sven Schmit <schmit@stanford.edu>
2015-05-26 15:56:31 -0700

    add covertype data

--- get_data.sh
+++ get_data.sh
@@ -9,7 +9,6 @@ curl http://www.pjreddie.com/media/files/mnist_train.csv -o data/mnist/mnist_tra
 curl http://www.pjreddie.com/media/files/mnist_test.csv -o data/mnist/mnist_test.csv
 
 # Get covertype data
-# mkdir -p data/covertype
-# curl https://archive.ics.uci.edu/ml/machine-learning-databases/covertype/covtype.data.gz -o data/covertype/covtype.data.gz
-
-
+mkdir -p data/covertype
+curl http://stanford.edu/~schmit/misc/covtype/train.data -o data/covertype/train.data
+curl http://stanford.edu/~schmit/misc/covtype/test.data -o data/covertype/test.data
