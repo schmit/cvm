ce12e666a3a0d79f606c4118d2ff79d998d5b7b1
Lan Huong Nguyen <nlhuong90@gmail.com>
2015-05-26 19:59:03 -0700

    change coverSimple.py

--- bin/coverSimple.py
+++ bin/coverSimple.py
@@ -1,27 +1,28 @@
 import sys
-
+import time
+from sklearn import svm as sklearnSVM
 from cvm.svm import NuSVC, SVC
 from sklearn.cross_validation import train_test_split
+from cvm.cascade import readiterator
 
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.regression import LabeledPoint
 
-
-def filter2classes(line):
-	words = line.split(",")
-	return (int(words[-1]) < 3)
-
 def parseData(line):
     fields = line.strip().split(",")
-    label = int(fields[-1])
-    feature = [float(feature) for feature in fields[0: 9]] #fields[0: -1]
+    label = int(int(fields[-1]) == 2)
+    feature = [float(feature) for feature in fields[: -1]] 
     return LabeledPoint(label, feature)
 
 
 if __name__ == "__main__":
+    C1 = 100.0
+    gamma1 = 10.0
+    nmax1 = 20000
+
     if (len(sys.argv) != 1):
         print "Usage: [usb root directory]/spark/bin/spark-submit --driver-memory 2g " + \
-            "mnist.py"
+            "./bin/covertype.py"
         sys.exit(1)
 
     # set up environment
@@ -31,21 +32,30 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf, batchSize=10)
 
     print 'Parsing data'
-    data = sc.textFile('data/covertype/scaled_covtype.data').collect()
-    
-    # Subsample and split data aproximately into training (60%) and test (40%)
-    data, throwaway = train_test_split(data, train_size = 0.1)    
-    small_train, small_test = train_test_split(data, train_size = 0.6)    
+    data = sc.textFile('../data/covertype/shuffle_scaled_covtype.data').collect()
+   
+    Split data aproximately into training (60%) and test (40%)
+    keep, throwaway = train_test_split(data, train_size = 0.2, random_state=0)  
+    train, test = train_test_split(keep, train_size = 0.6, random_state=0)    
 
-    small_trainRDD = sc.parallelize(small_train).filter(filter2classes).map(parseData).cache()
-    small_testRDD = sc.parallelize(small_test).filter(filter2classes).map(parseData).cache()
+    small_trainRDD = sc.parallelize(train).map(parseData).cache()    #sc.textFile('../data/covertype/trainCovtype.data').map(parseData).cache()
+    small_testRDD = sc.parallelize(test).map(parseData).cache()     #sc.textFile('../data/covertype/testCovtype.data').map(parseData).cache()
 
     print "Size of downsampled train set: ", small_trainRDD.count()
     print "Size of downsapled test set: ", small_testRDD.count()
 
-    print 'Fitting simple model'
-    simple_svm = SVC(C = 128.0, gamma=8.0)
-    simple_svm.simple_train(small_trainRDD)
+    print 'Fitting a simple model on heavily downsapled data'
+    print("Model parameters: C=" + str(C1) + "; gamma=" + str(gamma1))
+    #simple_svm = SVC(C=C1, gamma=gamma1)
+    X, y = readiterator(small_trainRDD.collect())
+    simple_svm = sklearnSVM.SVC(C=C1, gamma=gamma1)
+    
+    start = time.time()
+    #simple_svm.simple_train(small_trainRDD)
+    simple_svm.fit(X, y)
+    end = time.time()
+    print("Simple SVM train on " + str(small_trainRDD.count()) + " samples took " + str(end - start) + " time.")
+    print '[Simple Model] number of support vectors = ', len(simple_svm.support_)
 
     print 'Predicting outcomes small training set'
     simple_labelsAndPredsTrain = small_trainRDD.map(lambda p: (p.label, simple_svm.predict(p.features)))
@@ -59,3 +69,4 @@ if __name__ == "__main__":
 
     # clean up
     sc.stop()
+
--- bin/mnist.py
+++ bin/mnist.py
@@ -36,14 +36,10 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf, batchSize=10)
 
     print 'Parsing data'
-<<<<<<< HEAD
-    trainRDD = sc.textFile('../data/mnist/mnist_train.csv').map(lambda line: parseData(line, objective)).cache()
-    testRDD = sc.textFile('../data/mnist/mnist_test.csv').map(lambda line: parseData(line, objective)).cache()
-=======
+
     trainRDD = sc.textFile('data/mnist/train.csv').map(lambda line: parseData(line, objective)).cache()
     testRDD = sc.textFile('data/mnist/test.csv').map(lambda line: parseData(line, objective)).cache()
->>>>>>> 0e59322b79086c7a290a81cf6fa9c6c43a02f1e5
-
+    
     print 'Fitting model'
     # model = SVC(gamma=0.02, C=1.0, nmax=2000)
     model = KernelLogisticRegression(gamma=0.01, C=2.0, nmax=3000)
