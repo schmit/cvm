72c5df094e3637c9b89a413f0686fc3e0040784a
Sven Schmit <schmit@stanford.edu>
2015-05-27 11:22:05 -0700

    update covertype

--- bin/covertype.py
+++ bin/covertype.py
@@ -6,14 +6,14 @@ from pyspark import SparkConf, SparkContext
 from pyspark.mllib.regression import LabeledPoint
 
 C = 100.0
-gamma = 0.6
-nmax = 10000
+gamma = 0.2
+nmax = 20000
 
 
 def parseData(line):
     fields = line.strip().split(",")
     label = int(fields[-1])
-    feature = [float(feature)*3 for feature in fields[:10]] + [float(feature) for feature in fields[10:-1]] #fields[0: -1]
+    feature = [float(feature)*5 for feature in fields[:10]] + [float(feature) for feature in fields[10:-1]] #fields[0: -1]
     return LabeledPoint(1*(label==2), feature)
 
 
