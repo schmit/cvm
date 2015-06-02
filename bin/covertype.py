c54c5ce3c59b9bd1f56d2cec4c88398db16cbaec
Sven sharedmem <schmit@stanford.edu>
2015-06-02 15:02:23 -0700

    updated covertype and mnist scripts

--- bin/covertype.py
+++ bin/covertype.py
@@ -1,9 +1,12 @@
 import sys
 
 from cvm.svm import SVC
+from cvm.kreg import KernelLogisticRegression
 
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.regression import LabeledPoint
+
+from sklearn.cross_validation import train_test_split
 
 C = 100.0
 gamma = 0.2
@@ -26,13 +29,14 @@ if __name__ == "__main__":
     # set up environment
     conf = SparkConf() \
         .setAppName("Cascade") \
-        .set("spark.executor.memory", "2g")
+        .set("spark.executor.memory", "8g")
     sc = SparkContext(conf=conf, batchSize=10)
 
     print 'Parsing data'
 
-    trainRDD = sc.textFile('data/covertype/train.data', 24).map(parseData).cache()
-    testRDD = sc.textFile('data/covertype/test.data', 24).map(parseData).cache()
+    trainRDD = sc.textFile('data/covertype/train.data').map(parseData).cache()
+    
+    testRDD = sc.textFile('data/covertype/test.data', 24).map(parseData).cache() 
 
     print trainRDD.first().label, trainRDD.first().features
 
@@ -40,16 +44,17 @@ if __name__ == "__main__":
     print "Number of test samples: ", testRDD.count()
 
     print 'Fitting model'
-    svm = SVC(gamma=gamma, C=C, nmax=nmax)
-    svm.train(trainRDD)
+    model = SVC(gamma=gamma, C=C, nmax=nmax)
+    # model = KernelLogisticRegression(gamma=gamma, C=C, nmax=nmax)
+    model.train(trainRDD)
 
     print 'Predicting outcomes training set'
-    labelsAndPredsTrain = trainRDD.map(lambda p: (p.label, svm.predict(p.features)))
+    labelsAndPredsTrain = trainRDD.map(lambda p: (p.label, model.predict(p.features)))
     trainErr = labelsAndPredsTrain.filter(lambda (v, p): v != p).count() / float(trainRDD.count())
     print("Training Error = " + str(trainErr))
 
     print 'Predicting outcomes test set'
-    labelsAndPredsTest = testRDD.map(lambda p: (p.label, svm.predict(p.features)))
+    labelsAndPredsTest = testRDD.map(lambda p: (p.label, model.predict(p.features)))
     testErr = labelsAndPredsTest.filter(lambda (v, p): v != p).count() / float(testRDD.count())
     print("Test Error = " + str(testErr))
 
--- bin/mnist.py
+++ bin/mnist.py
@@ -6,6 +6,7 @@ Run using:
 '''
 
 import sys
+import time
 
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.regression import LabeledPoint
@@ -13,6 +14,10 @@ from pyspark.mllib.regression import LabeledPoint
 from cvm.svm import SVC
 from cvm.kreg import KernelLogisticRegression
 
+# Set parameters here:
+NMAX = 10000
+GAMMA = 0.02
+C = 1.0
 
 def objective(x):
     # prediction objective
@@ -32,28 +37,33 @@ if __name__ == "__main__":
     # set up environment
     conf = SparkConf() \
         .setAppName("Cascade") \
-        .set("spark.executor.memory", "2g")
+        .set("spark.executor.memory", "2g") \
+        .set("spark.kryoserializer.buffer.mb", "128")
     sc = SparkContext(conf=conf, batchSize=10)
 
     print 'Parsing data'
+    time_start = time.time()
     trainRDD = sc.textFile('data/mnist/train.csv').map(lambda line: parseData(line, objective)).cache()
     testRDD = sc.textFile('data/mnist/test.csv').map(lambda line: parseData(line, objective)).cache()
 
     print 'Fitting model'
-    # model = SVC(gamma=0.02, C=1.0, nmax=2000)
-    model = KernelLogisticRegression(gamma=0.01, C=2.0, nmax=3000)
+    model = SVC(gamma=GAMMA, C=C, nmax=NMAX)
+    # model = KernelLogisticRegression(gamma=0.01, C=2.0, nmax=3000)
 
     model.train(trainRDD)
+    print("Time: {:2.2f}".format(time.time() - time_start))
 
     print 'Predicting outcomes training set'
     labelsAndPredsTrain = trainRDD.map(lambda p: (p.label, model.predict(p.features)))
     trainErr = labelsAndPredsTrain.filter(lambda (v, p): v != p).count() / float(trainRDD.count())
     print("Training Error = " + str(trainErr))
+    print("Time: {:2.2f}".format(time.time() - time_start))
 
     print 'Predicting outcomes test set'
     labelsAndPredsTest = testRDD.map(lambda p: (p.label, model.predict(p.features)))
     testErr = labelsAndPredsTest.filter(lambda (v, p): v != p).count() / float(testRDD.count())
     print("Test Error = " + str(testErr))
+    print("Time: {:2.2f}".format(time.time() - time_start))
 
     # clean up
     sc.stop()
